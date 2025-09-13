63ab225f071940142b891b497d62afd384b41742
strubium <113206902+strubium@users.noreply.github.com>
2025-09-13 13:41:42 -0500

    Remove imports

--- src/main/java/com/strubium/openengie/OpenEngineering.java
+++ src/main/java/com/strubium/openengie/OpenEngineering.java
@@ -2,20 +2,9 @@ package com.strubium.openengie;
 
 import com.strubium.immersiveengineering.Tags;
 import com.strubium.openengie.assets.RuntimeAssets;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -38,6 +27,4 @@ public class OpenEngineering {
             RuntimeAssets.registerGeneratedResourcePack();
         }
     }
-
-
 }
